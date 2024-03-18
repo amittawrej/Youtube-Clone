58ad8fc7c1138329e44704a7aa2971916624b1f7
Amit Tawrej <121280979+amittawrej@users.noreply.github.com>
2024-03-18 17:51:47 +0530

    Update youtube.jsx

--- src/constants/youtube.jsx
+++ src/constants/youtube.jsx
@@ -1,7 +1,1 @@
-export const BASE_URL="https://www.googleapis.com/youtube/v3"
 
-export const API_KEY="AIzaSyCzLzAnvVp5GOSqO0IUidEQSJKjnc3GuhQ" //my abaddsha yt api key
-
-export const YOUTUBE_VIDEO_API='https://www.googleapis.com/youtube/v3/videos'
-
-// https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id={videoId}&regionCode=US&key=${API_KEY}
